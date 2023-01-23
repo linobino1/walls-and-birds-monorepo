9203323cf73a7e569bafa2d4e708b9062accb04b
Leo Hilsheimer <leo.hilsheimer@gmail.com>
2023-01-23 01:06:54 +0100

    CORS for www and non-www frontend domain

--- backend/src/payload.config.ts
+++ backend/src/payload.config.ts
@@ -3,11 +3,18 @@ import path from 'path';
 import Users from './collections/Users';
 import Shows from './collections/Shows';
 
+let cors: string[] = [];
+if (process.env.PAYLOAD_PUBLIC_CORS) {
+  cors = cors.concat(
+    process.env.PAYLOAD_PUBLIC_CORS.split(',').map((s) => s.trim())
+  );
+} else {
+  cors.push('http://localhost:5173');
+}
+
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_PUBLIC_HOST_BACKEND || 'http://localhost:3000',
-  cors: [
-    process.env.PAYLOAD_PUBLIC_HOST_FRONTEND || 'http://localhost:5173',
-  ],
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000',
+  cors,
   admin: {
     user: Users.slug,
   },
