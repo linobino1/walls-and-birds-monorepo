84041302a84aa63720cfb076e8fe467390dd3c76
Leo <leo.hilsheimer@gmail.com>
2023-06-13 17:20:33 +0200

    update info

--- frontend/src/views/info/index.vue
+++ frontend/src/views/info/index.vue
@@ -39,7 +39,24 @@ const info = computed(() => result?.value?.Info)
 
     <section>
       <h2>Info</h2>
-      <PressText>"Less than a kilometer away" is Walls & Birds new three song release on the Berlin record label Beatbude. Available now on vinyl, .wav and video. join in for news and shows: wallsandbirds.com</PressText>
+      <PressText>Walls & Birds is a german pop group. Their self-released tapes “Daytona Beach” (2015) and “Fitness Lady Wellness” (2017) gained critical acclaim in the mid 2010s eventually leading to a devoted fanbase worldwide. Their enigmatic and often extraordinary liveshows conjoining poetry, humour, seriousness and amusement, make them lovely to everyone who can let go into this kind of (atmo-)sphere.</PressText>
+      <ul>
+        <li>
+          web: <a href="https://wallsandbirds.com" target="_blank">wallsandbirds.com</a>
+        </li>
+        <li>
+          bandcamp: <a href="https://wallsandbirds.bandcamp.com" target="_blank">wallsandbirds.bandcamp.com</a>
+        </li>
+        <li>
+          youtube: <a href="https://www.youtube.com/@wallsandbirds" target="_blank">youtube.com/@wallsandbirds</a>
+        </li>
+        <li>
+          instagram: not available
+        </li>
+        <li>
+          label: <a href="https://beatbude.com" target="_blank">beatbude.com</a>
+        </li>
+      </ul>
     </section>
 
     <section>
@@ -51,7 +68,7 @@ const info = computed(() => result?.value?.Info)
       <PressText>2023: Walls & Birds is coming back with a new record on the Berlin record label Beatbude. The two song vinyl release with a limited first edition of 250 copies is accompanied by a digital bonus track and a consecutive music video. Join in for news and shows at <a href="https://wallsandbirds.com">wallsandbirds.com</a>.</PressText>
       <h3>Press Release (long)</h3>
       <PressText>
-        2023: Walls & Birds is coming back with a new record on the berlin record label beatbude. Much like their previous releases, "Less than a kilometer away" is Walls & Birds new take on popular music today. Recorded with the aid of "The Whitest Boy Alive" keyboarder Daniel Nentwig at the local Butterama studios, the three song release features a limited edition of 250 7" vinyl copys, a fold-out poster and a consecutive music video.The first song "Wearing a dress" touches on the theme of modern clothing habits, while questioning relationship patterns still widespread in popular music lyrics. The surpising element of new found love is also one of the themes of the B-Side “Heads || Tales ? '❤️' : '' telling the story of a late night after work taxi ride with the chorus contemplating about the eternal either or question, plainly answered with: love. The cheerful upbeat digital bonus track "Summer 2002" is as meaningless as any really nice pop song up to this day. 
+        2023: Walls & Birds is coming back with a new record on the berlin record label beatbude. Much like their previous releases, "Less than a kilometer away" is Walls & Birds new take on popular music today. Recorded with the aid of "The Whitest Boy Alive" keyboarder Daniel Nentwig at the local Butterama studios, the three song release features a limited edition of 250 7" vinyl copys, a fold-out poster and a consecutive music video.The first song "Wearing a dress" touches on the theme of modern clothing habits, while questioning relationship patterns still widespread in popular music lyrics. The surpising element of new found love is also one of the themes of the B-Side “heads || tales ? '❤️' : ''“ telling the story of a late night after work taxi ride with the chorus contemplating about the eternal either or question, plainly answered with: love. The cheerful upbeat digital bonus track "Summer 2002" is as meaningless as most any nice pop song to date. 
         Walls & Birds are known for their smooth and dreamy yet amiable approach to both instruments and audience, a combination of poetry, humour, seriousness and amusement, which makes them lovely to everyone who can let go into this kind of (atmo-)sphere. 
       </PressText>
     </section>
