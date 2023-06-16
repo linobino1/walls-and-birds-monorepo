236e86a2f4102d284e2f6fef7f3bd126e193562c
Leo <leo.hilsheimer@gmail.com>
2023-06-16 14:44:39 +0200

    fix typos

--- frontend/src/views/info/index.vue
+++ frontend/src/views/info/index.vue
@@ -60,9 +60,9 @@ const info = computed(() => result?.value?.Info)
     </section>
 
     <section>
-      <h2>Latest Release "Less than a kilometer away"</h2>
+      <h2>Latest Release ”Less than a kilometer away”</h2>
       <p>
-        release date: tba, 2023
+        release date: July 21st, 2023
       </p>
       <ul>
         <li>
@@ -73,10 +73,10 @@ const info = computed(() => result?.value?.Info)
         </li>
       </ul>
       <h3>Press Release (short)</h3>
-      <PressText>2023: Walls & Birds is coming back with a new record on the Berlin record label Beatbude. The two song vinyl release with a limited first edition of 250 copies is accompanied by a digital bonus track and a consecutive music video. Join in for news and shows at <a href="https://wallsandbirds.com">wallsandbirds.com</a>.</PressText>
+      <PressText>2023: Walls & Birds is coming back with a new record on the Berlin record label Beatbude. The two song vinyl release with a limited first edition of 300 copies is accompanied by a digital bonus track and a consecutive music video. Join in for news and shows at <a href="https://wallsandbirds.com">wallsandbirds.com</a>.</PressText>
       <h3>Press Release (long)</h3>
       <PressText>
-        2023: Walls & Birds is coming back with a new record on the berlin record label beatbude. Much like their previous releases, "Less than a kilometer away" is Walls & Birds new take on popular music today. Recorded with the aid of "The Whitest Boy Alive" keyboarder Daniel Nentwig at the local Butterama studios, the three song release features a limited edition of 250 7" vinyl copys, a fold-out poster and a consecutive music video.The first song "Wearing a dress" touches on the theme of modern clothing habits, while questioning relationship patterns still widespread in popular music lyrics. The surpising element of new found love is also one of the themes of the B-Side “heads || tales ? '❤️' : ''“ telling the story of a late night after work taxi ride with the chorus contemplating about the eternal either or question, plainly answered with: love. The cheerful upbeat digital bonus track "Summer 2002" is as meaningless as most any nice pop song to date. 
+        2023: Walls & Birds is coming back with a new record on the berlin record label Beatbude. Much like their previous releases, ”Less than a kilometer away” is Walls & Birds new take on popular music today. Recorded with the aid of ”The Whitest Boy Alive” keyboarder Daniel Nentwig at the local Butterama Recording Center, the three song release features a limited edition of 300 7" vinyl copies, a fold-out poster and a consecutive music video. The first song ”Wearing a dress” touches on the theme of modern clothing habits, while questioning relationship patterns still widespread in popular music lyrics. The surprising element of new found love is also one of the themes of the B-side “heads || tales ? '❤️'“ telling the story of a late night after work taxi ride with the chorus contemplating about the eternal either or question, plainly answered with: love. The cheerful upbeat digital bonus track ”Summer 2002” is as meaningless as most any nice pop song to date. 
         Walls & Birds are known for their smooth and dreamy yet amiable approach to both instruments and audience, a combination of poetry, humour, seriousness and amusement, which makes them lovely to everyone who can let go into this kind of (atmo-)sphere. 
       </PressText>
     </section>
